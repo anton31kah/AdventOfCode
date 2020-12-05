9c19c55c6e42bb4eb2442a4429e462bb9ee3c9a9
Anton Kahwaji <anton.kahwaji@gmail.com>
2020-12-05 13:43:14 +0100

    change

--- files-gen.ps1
+++ files-gen.ps1
@@ -1,4 +1,10 @@
-$filename = 'Day 04 Passport Processing'
+$day = Get-Date -Format "'Day' dd"
+$filename = "$day Binary Boarding"
+
+# New-Item -ItemType directory "$day" 
+# New-Item -ItemType file "$day/$filename (part 1).py"
+# New-Item -ItemType file "$day/$filename (part 2).py"
+# New-Item -ItemType file "$day/$filename.in.txt"
 
 New-Item -ItemType file "$filename (part 1).py"
 New-Item -ItemType file "$filename (part 2).py"
