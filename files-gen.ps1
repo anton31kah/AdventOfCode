c52eed21f060daca80a0bf507ada423a75ee3f08
Anton Kahwaji <anton.kahwaji@gmail.com>
2020-12-18 13:02:02 +0100

    changed initial code

--- files-gen.ps1
+++ files-gen.ps1
@@ -1,7 +1,7 @@
-$starterCode = "from src.common.common import get_lines`n`n`nlines = get_lines()`n"
+$starterCode = "from src.common.common import get_lines`n`n`ndef main():`n    lines = get_lines()`n`n`nif __name__ == ""__main__"":`n    main()`n"
 
 $day = Get-Date -Format "'day'dd"
-# $day = 'day04'
+# $day = 'dayX'
 
 New-Item -Path "src/$day" -Name "__init__.py" -ItemType "file" -Force
 New-Item -Path "src/$day" -Name "part1.py" -ItemType "file" -Value $starterCode
