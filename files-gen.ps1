fe7a468c7a299504fac18989e1ff3826167c4f25
anton31kah <anton.kahwaji@gmail.com>
2021-12-04 18:13:02 +0100

    Fix files generator

--- files-gen.ps1
+++ files-gen.ps1
@@ -6,3 +6,4 @@ New-Item -Path "src/$year/$day" -Name "__init__.py" -ItemType "file" -Force
 Get-Content -Raw .\files-gen.template.py | New-Item -Path "src/$year/$day" -Name "part1.py" -ItemType "file"
 Get-Content -Raw .\files-gen.template.py | New-Item -Path "src/$year/$day" -Name "part2.py" -ItemType "file"
 New-Item -Path "src/$year/$day" -Name "in.txt" -ItemType "file"
+New-Item -Path "src/$year/$day" -Name "inS.txt" -ItemType "file"
