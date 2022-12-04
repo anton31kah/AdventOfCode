868195a78c011c4f3729b930abb4a4082feabdfe
Anton Kahwaji <anton.kahwaji@gmail.com>
2022-12-04 09:18:14 +0000

    4/22

---
+++ src/year2022/day04/part1.py
@@ -0,0 +1,27 @@
+from src.common.common import get_lines
+
+
+def overlap(a, b):
+    a1, a2 = a
+    b1, b2 = b
+    return a1 >= b1 and a2 <= b2 or b1 >= a1 and b2 <= a2
+
+
+def main():
+    lines = get_lines()
+
+    total = 0
+
+    for line in lines:
+        r1, r2 = line.split(',')
+        r1 = list(map(int, r1.split('-')))
+        r2 = list(map(int, r2.split('-')))
+        
+        if overlap(r1, r2):
+            total += 1
+
+    print(total)
+
+
+if __name__ == "__main__":
+    main()
