db524fb8674e9f71d90d1523e4f1e865a8a2aa49
Anton Kahwaji <anton.kahwaji@gmail.com>
2022-12-15 06:08:10 +0000

    15/22 (p2 after 10 years)

--- src/year2022/day15/part2.py
+++ src/year2022/day15/part2.py
@@ -18,34 +18,24 @@ def main():
 
     sensors_beacons = [parse_line(line) for line in lines]
 
-    min_x_beacon = min(bx for _, (bx, _) in sensors_beacons)
-    max_x_beacon = max(bx for _, (bx, _) in sensors_beacons)
+    max_x = max_y = 4_000_000
 
-    y = 4000000
-
-    total = 0
-
-    print(min_x_beacon, max_x_beacon, len(range(min_x_beacon * 2, max_x_beacon * 2)))
-    for x in range(min_x_beacon * 2, max_x_beacon * 2):
-        if x % 100_000 == 0:
-            print(x, total)
-        position = (x, y)
-        in_range_of_sensor = False
-        for sensor, beacon in sensors_beacons:
-            dis_beacon = distance(sensor, beacon)
-            pos_beacon = distance(sensor, position)
-            if pos_beacon <= dis_beacon:
-                in_range_of_sensor = True
-        if in_range_of_sensor:
-            same_position_as_beacon = False
+    for x in range(0, max_x):
+        print('x', x)
+        for y in range(0, max_y):
+            if y % 100_000 == 0:
+                print('y', y)
+            position = (x, y)
+            in_range_of_sensor = False
             for sensor, beacon in sensors_beacons:
-                if position in (sensor, beacon):
-                    same_position_as_beacon = True
-                    break
-            if not same_position_as_beacon:
-                total += 1
+                dis_beacon = distance(sensor, beacon)
+                pos_beacon = distance(sensor, position)
+                if pos_beacon <= dis_beacon:
+                    in_range_of_sensor = True
+            if not in_range_of_sensor:
+                print(position)
 
-    print(total)
+    print(None)
 
 
 if __name__ == "__main__":
