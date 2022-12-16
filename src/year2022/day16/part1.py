ca03ecd1feb09aee62a03c4dafd191e08514dd19
Anton Kahwaji <anton.kahwaji@gmail.com>
2022-12-16 09:02:43 +0000

    16/22 (p1)

--- src/year2022/day16/part1.py
+++ src/year2022/day16/part1.py
@@ -1,9 +1,10 @@
+import math
 import re
 from src.common.common import get_lines
 
 
 class ValveNode:
-    def __init__(self, valve_id, flow, tunnels):
+    def __init__(self, valve_id: str, flow: int, tunnels: 'list[ValveNode]'):
         self.valve_id = valve_id
         self.flow = flow
         self.tunnels = tunnels
@@ -23,7 +24,7 @@ def parse_line(line):
     return valve, int(flow), tunnels.split(', ')
 
 
-def parse_input(lines):
+def parse_input(lines) -> list[ValveNode]:
     valves = {}
 
     for line in lines:
@@ -36,27 +37,75 @@ def parse_input(lines):
     for valve_id in list(valves.keys()):
         valves[valve_id] = valves[valve_id][0]
 
-    return valves
+    return list(valves.values())
 
 
-def find_max():
-    """
-    HH [22] => cost = 5+1=6 => value => 22*24=528
-    JJ [21] => cost = 2+1=3 => value => 21*27=567
-    DD [20] => cost = 1+1=2 => value => 20*28=560
-    BB [13] => cost = 1+1=2 => value => 13*28=364
-    EE  [3] => cost = 2+1=3 => value =>  3*27=81
-    CC  [2] => cost = 2+1=3 => value =>  2*27=54
-    """
-    ...
+def floyd_warshall(valves: list[ValveNode]) -> dict[tuple[str, str], int]:
+    dist = {}
+
+    for valve in valves:
+        for tunnel in valve.tunnels:
+            dist[(valve.valve_id, tunnel.valve_id)] = 1
+            dist[(tunnel.valve_id, valve.valve_id)] = 1
+        dist[(valve.valve_id, valve.valve_id)] = 0
+    for valve_k in valves:
+        k = valve_k.valve_id
+        for valve_i in valves:
+            i = valve_i.valve_id
+            for valve_j in valves:
+                j = valve_j.valve_id
+
+                dist.setdefault((i, j), math.inf)
+                dist.setdefault((i, k), math.inf)
+                dist.setdefault((k, j), math.inf)
+
+                if dist[(i, j)] > dist[(i, k)] + dist[(k, j)]:
+                    dist[(i, j)] = dist[(i, k)] + dist[(k, j)]
+
+    return dist
+
+
+def cached(fun):
+    cache = {}
+
+    def wrapper(valves, distances, time_left, open_valves, current_valve):
+        key = time_left, open_valves, current_valve
+        if key not in cache:
+            cache[key] = fun(valves, distances, time_left, open_valves, current_valve)
+        return cache[key]
+
+    return wrapper
+
+
+@cached
+def find_max(valves: list[ValveNode], distances: dict[tuple[str, str], int], time_left: int, open_valves: frozenset[str], current_valve: str):
+    if time_left < 0:
+        return 0
+
+    possible = []
+
+    for valve in valves:
+        if valve.valve_id not in open_valves and valve.flow > 0 and valve.valve_id != current_valve:
+            cost = distances[(valve.valve_id, current_valve)] + 1
+            value = find_max(valves, distances, time_left - cost, open_valves | {valve.valve_id}, valve.valve_id)
+            value += valve.flow * max(time_left - cost, 0)
+            possible.append(value)
+
+    if len(possible) == 0:
+        return 0
+
+    return max(possible)
 
 
 def main():
-    lines = get_lines('S')
+    lines = get_lines('')
 
     valves = parse_input(lines)
+    distances = floyd_warshall(valves)
 
-    print(valves)
+    value = find_max(valves, distances, 30, frozenset(), 'AA')
+
+    print(value)
 
 
 if __name__ == "__main__":
