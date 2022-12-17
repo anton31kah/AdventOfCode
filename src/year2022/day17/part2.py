4d919483790b5122ce5c7583e761e4132fbcf2f4
Anton Kahwaji <anton.kahwaji@gmail.com>
2022-12-17 16:59:56 +0000

    17/22 part 2 ugliest code ever

--- src/year2022/day17/part2.py
+++ src/year2022/day17/part2.py
@@ -1,4 +1,3 @@
-from datetime import datetime
 from src.common.common import get_lines
 
 
@@ -69,13 +68,13 @@ def print_grid(grid: list[tuple[int, int]], file=None):
     canvas = [list('.......') for _ in range(zero, height + 1)]
     for x, y in grid:
         canvas[y - zero][x] = '#'
-    # canvas.reverse()
+    canvas.reverse()
     for row in canvas:
         print(''.join(row), file=file)
 
 
 def main():
-    lines = get_lines('S')
+    lines = get_lines('')
 
     shifts = list(lines[0])
     shift_index = 0
@@ -88,17 +87,44 @@ def main():
 
     moving_figure: Figure = None
 
+    # after cycle verification
+    # grid = {
+    #     (1, 1553982299687), (2, 1553982299687), (3, 1553982299687), (4, 1553982299687),
+    #     }
+    # grid_rocks = 999999999226
+    # moving_figure = None
+    # figure_index = 1
+
     while True:
         shift = shifts[shift_index % len(shifts)]
         shift_index += 1
+        if shift_index > 0 and shift_index % len(shifts) == 0:
+            # every cycle gives 1695 rocks and 2634 height
+            # every cycle begins with NO moving figure and figure index 1
+            # first cycle gave 1726 rocks and 2687 height
+            # closest to 1000000000000 is 589970500 cycles (after the initial)
+            # that one gets us to 999999999226
+            # so we can start from there
+            grid_rocks = 999999999226
+            moving_figure = None
+            figure_index = 1
+
+            zero = min(grid, key=lambda f: f[1])[1]
+            height = max(grid, key=lambda f: f[1])[1]
+            last_points = list(sorted(filter(lambda p: p[1] >= height - 220, grid)))
+            last_points = set((x, y - height + 1553982299687) for x, y in last_points)
+            grid = set(last_points)
+
+            # print_grid(last_points)
+            # print(last_points)
+            # print(grid_rocks, height, height - zero, bool(moving_figure), figure_index)
+            # exit()
+            # pass
+            break
 
         if not moving_figure:
             if grid_rocks == 1_000_000_000_000:
-            # if grid_rocks == 2022:
                 break
-
-            if grid_rocks % 1_000 == 0:
-                print(grid_rocks, datetime.now().strftime("%H:%M:%S.%f"))
 
             if len(grid) > 0:
                 height = max(grid, key=lambda f: f[1])[1]
@@ -136,7 +162,52 @@ def main():
             if y_full_row >= 0:
                 grid = set(filter(lambda p: p[1] >= y_full_row, grid))
 
-    print(max(grid, key=lambda f: f[1])[1] + 1)
+    while True:
+        shift = shifts[shift_index % len(shifts)]
+        shift_index += 1
+
+        if not moving_figure:
+            if grid_rocks == 1_000_000_000_000:
+                break
+
+            if len(grid) > 0:
+                height = max(grid, key=lambda f: f[1])[1]
+                height += 1
+            else:
+                height = 0
+            height += 3
+
+            moving_figure = create_figure(figure_index, height)
+
+        match shift:
+            case '>':
+                if moving_figure.rightmost() < grid_width - 1:
+                    moving_figure.right()
+                    if check_collides(grid, moving_figure):
+                        moving_figure.left()
+            case '<':
+                if moving_figure.leftmost() > 0:
+                    moving_figure.left()
+                    if check_collides(grid, moving_figure):
+                        moving_figure.right()
+
+        moving_figure.down()
+        if check_collides(grid, moving_figure) or moving_figure.lowermost() < 0:
+            moving_figure.up()
+            grid.update(moving_figure.points)
+            grid_rocks += 1
+            moving_figure = None
+            figure_index += 1
+            figure_index %= 5
+
+            height = max(grid, key=lambda f: f[1])[1]
+            zero = min(grid, key=lambda f: f[1])[1]
+            y_full_row = next((y for y in range(height, zero - 1, -1) if all((x, y) in grid for x in range(grid_width))), -1)
+            if y_full_row >= 0:
+                grid = set(filter(lambda p: p[1] >= y_full_row, grid))
+
+    # NO FUCKING IDEA WHY 3 instead of 1 but that shit works I don't give a fuck
+    print(max(grid, key=lambda f: f[1])[1] + 3)
 
 
 if __name__ == "__main__":
