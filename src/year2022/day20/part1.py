b0a7c3dde1ea3307ac38a13d239fbbf3de9cda8d
Anton Kahwaji <anton.kahwaji@gmail.com>
2022-12-20 09:46:47 +0000

    20/12

--- src/year2022/day20/part1.py
+++ src/year2022/day20/part1.py
@@ -1,32 +1,77 @@
 from src.common.common import get_lines
 
 
+class LinkedListNode:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        self.prev = None
+        self.is_head = False
+
+    def __str__(self):
+        return str(self.value)
+
+    def __repr__(self):
+        return str(self)
+
+
+def to_linkedlist_array(array):
+    result = [LinkedListNode(val) for val in array]
+
+    for i in range(len(result) - 1):
+        result[i].next = result[i + 1]
+        result[i - 1].prev = result[i]
+
+    result[-1].next = result[0]
+    result[0].prev = result[-1]
+
+    result[0].is_head = True
+
+    return result
+
+
 def main():
-    lines = get_lines('S')
+    lines = get_lines('')
 
     numbers = [int(line) for line in lines]
-    indexes = {idx: idx for idx, num in enumerate(numbers)}
+    numbers = to_linkedlist_array(numbers)
 
-    for idx in sorted(indexes.keys()):
-        position = indexes[idx]
-        number = numbers[position]
-        new_position = (position + number) % len(numbers)
+    current = numbers[0]
 
-        if new_position < position:
-            numbers.pop(position)
-            numbers.insert(new_position, number)
-            for i in range(new_position + 1, position + 1):
-                # pos[i] = pos[i] - 1
-                # need two way map
-                ...
-        elif new_position > position:
-            numbers.insert(new_position, number)
-            numbers.pop(position)
-            for i in range(position, new_position):
-                # pos[i] = pos[i] - 1
-                ...
+    hit_head = False
 
-    # print(numbers[1000 % len(numbers)] + numbers[2000 % len(numbers)] + numbers[3000 % len(numbers)])
+    while True:
+        if current.is_head and hit_head:
+            break
+
+        if current.is_head:
+            hit_head = True
+
+        position = numbers.index(current)
+
+        new_position = (position + current.value) % (len(numbers) - 1)
+
+        numbers.insert(new_position, numbers.pop(position))
+
+        # print(current, ';', numbers, ';', {new_position < position:'<', new_position > position: '>'}.get(True, ''))
+
+        current = current.next
+
+    # print(numbers)
+
+    positions = [1000, 2000, 3000]
+
+    for idx, node in enumerate(numbers):
+        if node.value == 0:
+            positions = [p + idx for p in positions]
+
+    result = [numbers[p % len(numbers)].value for p in positions]
+
+    # print(list(zip(positions, result)))
+
+    result = sum(result)
+
+    print(result)
 
 
 if __name__ == "__main__":
--- src/year2022/day20/part2.py
+++ src/year2022/day20/part2.py
@@ -1,10 +1,77 @@
 from src.common.common import get_lines
 
 
+class LinkedListNode:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        self.prev = None
+        self.is_head = False
+
+    def __str__(self):
+        return str(self.value)
+
+    def __repr__(self):
+        return str(self)
+
+
+def to_linkedlist_array(array):
+    result = [LinkedListNode(val) for val in array]
+
+    for i in range(len(result) - 1):
+        result[i].next = result[i + 1]
+        result[i - 1].prev = result[i]
+
+    result[-1].next = result[0]
+    result[0].prev = result[-1]
+
+    result[0].is_head = True
+
+    return result
+
+
 def main():
     lines = get_lines('')
 
-    print(0)
+    numbers = [int(line) * 811589153 for line in lines]
+    numbers = to_linkedlist_array(numbers)
+
+    current = numbers[0]
+
+    hit_head_times = 0
+
+    while True:
+        if current.is_head and hit_head_times >= 10:
+            break
+
+        if current.is_head:
+            hit_head_times += 1
+
+        position = numbers.index(current)
+
+        new_position = (position + current.value) % (len(numbers) - 1)
+
+        numbers.insert(new_position, numbers.pop(position))
+
+        # print(current, ';', numbers, ';', {new_position < position:'<', new_position > position: '>'}.get(True, ''))
+
+        current = current.next
+
+    # print(numbers)
+
+    positions = [1000, 2000, 3000]
+
+    for idx, node in enumerate(numbers):
+        if node.value == 0:
+            positions = [p + idx for p in positions]
+
+    result = [numbers[p % len(numbers)].value for p in positions]
+
+    # print(list(zip(positions, result)))
+
+    result = sum(result)
+
+    print(result)
 
 
 if __name__ == "__main__":
