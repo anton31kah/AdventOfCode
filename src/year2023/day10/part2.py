0fcf5b1e959ade4a0ba05d01df8e6b200957bcb2
Anton Kahwaji <anton.kahwaji@gmail.com>
2023-12-10 12:06:23 +0000

    2023/12/10 (part1)

---
+++ src/year2023/day10/part2.py
@@ -0,0 +1,199 @@
+from src.common.common import get_lines
+
+
+def pretty_print(lines, in_loop=None):
+    for y, line in enumerate(lines):
+        new_line = line.replace('|', '│').replace('-', '─').replace('L', '└').replace('.', '.').replace('F', '┌').replace('J', '┘').replace('7', '┐')
+
+        if in_loop is not None:
+            new_line_builder = list(new_line)
+            for x, c in enumerate(new_line):
+                if c == '.':
+                    if (x, y) in in_loop:
+                        new_line_builder[x] = 'I'
+                    else:
+                        new_line_builder[x] = 'O'
+            new_line = ''.join(new_line_builder)
+
+        print(new_line)
+
+    print()
+
+
+def find_start(lines):
+    for y in range(len(lines)):
+        for x in range(len(lines[y])):
+            if lines[y][x] == 'S':
+                return (x, y)
+    return None
+
+
+
+horizontally_connected = {
+    ('-','-'),
+    ('-','J'),
+    ('F','J'),
+    ('L','J'),
+    ('L','J'),
+    ('L','7'),
+    ('L','-'),
+    ('F','7'),
+    ('F','J'),
+    ('F','-'),
+    ('-','7'),
+    ('L','7'),
+    ('F','7'),
+}
+vertically_connected = {
+    ('|','|'),
+    ('F','J'),
+    ('7','J'),
+    ('|','J'),
+    ('7','L'),
+    ('F','L'),
+    ('|','L'),
+    ('F','J'),
+    ('F','L'),
+    ('F','|'),
+    ('7','J'),
+    ('7','L'),
+    ('7','|'),
+}
+def are_connected(lines, point1, point2):
+    x1, y1 = point1
+    x2, y2 = point2
+    
+    if abs(x1 - x2) + abs(y1 - y2) != 1:
+        return False
+
+    c1 = lines[y1][x1]
+    c2 = lines[y2][x2]
+
+    diff_y = y1 - y2
+    diff_x = x1 - x2
+
+    match (diff_x, diff_y):
+        case (1, 0): # p1 is to the right of p2
+            return (c2, c1) in horizontally_connected
+        case (-1, 0): # p1 is to the left of p2
+            return (c1, c2) in horizontally_connected
+        case (0, 1): # p1 is below p2
+            return (c2, c1) in vertically_connected
+        case (0, -1): # p1 is above p2
+            return (c1, c2) in vertically_connected
+
+    raise ValueError("Invalid diff " + (diff_x, diff_y))
+
+
+def find_neighbors(lines, point, visited=None):
+    neighbors = []
+    x, y = point
+    points = [
+        (x - 1, y),
+        (x, y - 1),
+        (x, y + 1),
+        (x + 1, y),
+        ]
+    for x, y in points:
+        if y >= 0 and y < len(lines):
+            line = lines[y]
+            if x >= 0 and x < len(line):
+                neighbor = x, y
+                if are_connected(lines, point, neighbor) and (visited is None or neighbor not in visited):
+                    neighbors.append(neighbor)
+    return neighbors
+
+
+def determine_best_start_pipe(lines, start_point):
+    x, y = start_point
+    lines_copy = [line[:] for line in lines]
+    for pipe in '7FLJ-|':
+        lines_copy[y] = lines[y].replace('S', pipe)
+        neighbors = find_neighbors(lines_copy, start_point)
+        if len(neighbors) == 2:
+            return pipe
+    return None
+
+
+def get_loop_points(lines, start_point):
+    distance = 0
+    visited = {start_point}
+
+    point1, point2 = find_neighbors(lines, start_point)
+
+    visited.add(point1)
+    visited.add(point2)
+    distance += 1
+
+    while True:
+        if point1 == point2 and distance > 0:
+            break
+
+        neighbors1 = find_neighbors(lines, point1, visited)
+        point1 = neighbors1[0]
+
+        neighbors2 = find_neighbors(lines, point2, visited)
+        point2 = neighbors2[0]
+
+        visited.add(point1)
+        visited.add(point2)
+
+        distance += 1
+
+        if len(neighbors1) != 1 and len(neighbors2) != 1:
+            raise ValueError("Invalid state")
+
+    return visited
+
+
+def count_ray_case_intersections(lines, point, loop_points):
+    if point in loop_points:
+        raise ValueError("Initial point on loop edge " + point)
+
+    count = 0
+
+    x, y = point
+
+    for i in range(x + 1, len(lines[y])):
+        current = x + i, y
+        if current in loop_points and lines[y][x + i] != '-':
+            count += 1
+
+    return count
+
+
+def find_points_in_loop(lines, loop_points):
+    in_loop = set()
+
+    for y, line in enumerate(lines):
+        for x, c in enumerate(line):
+            current = x, y
+            if c == '.':
+                num = count_ray_case_intersections(lines, current, loop_points)
+                if num % 2 == 1:
+                    in_loop.add(current)
+
+    return in_loop
+
+
+def main():
+    lines = get_lines('S3')
+    
+    pretty_print(lines)
+
+    start_point = find_start(lines)
+    
+    start_point_pipe = determine_best_start_pipe(lines, start_point)
+    lines[start_point[1]] = lines[start_point[1]].replace('S', start_point_pipe)
+    
+    loop = get_loop_points(lines, start_point)
+
+    points_in_loop = find_points_in_loop(lines, loop)
+    
+    pretty_print(lines, points_in_loop)
+
+    print(len(points_in_loop))
+
+
+if __name__ == "__main__":
+    main()
